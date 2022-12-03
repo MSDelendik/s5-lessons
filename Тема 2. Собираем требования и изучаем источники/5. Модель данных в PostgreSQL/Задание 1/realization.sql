397d703803b5a0bf2665d2a3678b3008a107ca7b
MSDelendik <90086686+MSDelendik@users.noreply.github.com>
2022-12-03 21:03:18 +0300

    Task2_commit_2

--- Тема 2. Собираем требования и изучаем источники/5. Модель данных в PostgreSQL/Задание 1/realization.sql
+++ Тема 2. Собираем требования и изучаем источники/5. Модель данных в PostgreSQL/Задание 1/realization.sql
@@ -1,2 +1,3 @@
-select distinct ((event_value::JSON ->> 'product_payments')::JSON->>1)::JSON->>'product_name'
-from public.outbox;
+select distinct ((event_value::JSON ->> 'product_payments')::JSON->>1)::JSON->>'product_name' as product_name
+from public.outbox
+where ((event_value::JSON ->> 'product_payments')::JSON->>1)::JSON->>'product_name' is not null;
