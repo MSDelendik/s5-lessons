81b646a3d4215907d3530b6cd708a3da02d17ed4
MSDelendik <90086686+MSDelendik@users.noreply.github.com>
2022-12-10 15:45:16 +0300

    task_3_7_2

--- Тема 3. Проектирование DWH/7. Хранение исторических данных/Задание 2/realization.sql
+++ Тема 3. Проектирование DWH/7. Хранение исторических данных/Задание 2/realization.sql
@@ -1,20 +1,24 @@
 -- Удалите внешний ключ из sales
-...
+ALTER TABLE public.sales DROP CONSTRAINT IF EXISTS sales_products_product_id_fk;
 
 -- Удалите первичный ключ из products
-...
+ALTER TABLE public.products DROP CONSTRAINT IF EXISTS products_pk;
 
 -- Добавьте новое поле id для суррогантного ключа в products
-...
+ALTER TABLE public.products
+ADD COLUMN IF NOT EXISTS id serial PRIMARY KEY,
+ADD COLUMN IF NOT EXISTS valid_from timestamptz,
+ADD COLUMN IF NOT EXISTS valid_to timestamptz
+;
 
 -- Сделайте данное поле первичным ключом
-...
+
 
 -- Добавьте дату начала действия записи в products
-...
+
 
 -- Добавьте дату окончания действия записи в products
-...
+
 
 -- Добавьте новый внешний ключ sales_products_id_fk в sales
-...
+ALTER TABLE public.sales ADD CONSTRAINT sales_products_id_fk FOREIGN KEY (product_id) REFERENCES products (id);
