1b24bb9d03c41a4f23238fae725d19fb0858c5cc
MSDelendik <90086686+MSDelendik@users.noreply.github.com>
2022-12-24 16:08:48 +0300

    task_4_5_4(6)

--- dags/examples/stg/bonus_system_ranks_dag/events_loader.py
+++ dags/examples/stg/bonus_system_ranks_dag/events_loader.py
@@ -65,7 +65,7 @@ class EventDestRepository:
 class EventLoader:
     WF_KEY = "example_event_origin_to_stg_workflow"
     LAST_LOADED_ID_KEY = "last_loaded_id"
-    BATCH_LIMIT = 100  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
+    BATCH_LIMIT = 10000  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
 
     def __init__(self, pg_origin: PgConnect, pg_dest: PgConnect, log: Logger) -> None:
         self.pg_dest = pg_dest
