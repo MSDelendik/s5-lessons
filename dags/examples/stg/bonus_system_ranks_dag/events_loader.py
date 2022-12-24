b2934851187c5a622573be548c1988c11d5c861a
MSDelendik <90086686+MSDelendik@users.noreply.github.com>
2022-12-24 16:05:24 +0300

    task_4_5_4(5)

--- dags/examples/stg/bonus_system_ranks_dag/events_loader.py
+++ dags/examples/stg/bonus_system_ranks_dag/events_loader.py
@@ -49,8 +49,8 @@ class EventDestRepository:
                     VALUES (%(id)s, %(event_ts)s, %(event_type)s, %(event_value)s)
                     ON CONFLICT (id) DO UPDATE
                     SET
-                        event_ts = EXCLUDED.event_ts
-                        event_type = EXCLUDED.event_type
+                        event_ts = EXCLUDED.event_ts,
+                        event_type = EXCLUDED.event_type,
                         event_value = EXCLUDED.event_value;
                 """,
                 {
