e032cdd4c09575b5069f125e1dbd45266d20780e
MSDelendik <90086686+MSDelendik@users.noreply.github.com>
2022-12-25 15:59:00 +0300

    task_4_7_2(10)

--- dags/examples/dds/dm_users.sql
+++ dags/examples/dds/dm_users.sql
@@ -1,14 +1,13 @@
-INSERT INTO dds.dm_users
+INSERT INTO dds.dm_users (user_id, user_name, user_login)
 SELECT
-id,
 object_value::JSON ->> '_id' AS user_id,
 object_value::JSON ->> 'name' AS user_name,
-object_value::JSON ->> 'login' AS user_login,
+object_value::JSON ->> 'login' AS user_login
 from stg.ordersystem_users
 WHERE object_value::JSON ->> 'login' IS NOT NULL
 ORDER BY 1
 ON CONFLICT (id) DO UPDATE
 SET
-user_id = EXCLUDED.user_id
-user_name = EXCLUDED.user_name
+user_id = EXCLUDED.user_id,
+user_name = EXCLUDED.user_name,
 user_login = EXCLUDED.user_login;
