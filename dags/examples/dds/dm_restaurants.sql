139ddc29bc6488ffb2bec8573a887d1e917454b5
MSDelendik <90086686+MSDelendik@users.noreply.github.com>
2022-12-25 17:28:47 +0300

    task_4_7_3(2)

--- dags/examples/dds/dm_restaurants.sql
+++ dags/examples/dds/dm_restaurants.sql
@@ -1,8 +1,9 @@
-INSERT INTO dds.dm_restaurants (restaurant_id, restaurants_name, active_from, active_to)
+INSERT INTO dds.dm_restaurants (restaurant_id, restaurant_name, active_from, active_to)
 SELECT 
 object_value::JSON ->> '_id' AS restaurant_id,
 object_value::JSON ->> 'name' AS restaurant_name,
 object_value::JSON ->> 'update_ts' AS actual_from,
 '2099-12-31'::timestamp AS actual_to
 FROM stg.ordersystem_restaurants
-WHERE object_value::JSON ->> 'menu' IS NOT null;
+WHERE object_value::JSON ->> 'menu' IS NOT null
+AND object_value::JSON ->> '_id' NOT IN (SELECT DISTINCT restaurant_id FROM dds.dm_restaurants);;
