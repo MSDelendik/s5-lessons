4b04909c38df0cec5dddc87166493979efe5f6f9
MSDelendik <90086686+MSDelendik@users.noreply.github.com>
2023-01-13 22:09:23 +0300

    task_5_1

---
+++ Тема 5. Тестирование, интеграция и документация решения/2. Тестирование DWH/Задание 1/realization.sql
@@ -0,0 +1,13 @@
+SELECT * FROM public_test.dm_settlement_report_actual a
+FULL JOIN public_test.dm_settlement_report_expected b ON 
+a.restaurant_id=b.restaurant_id 
+AND a.restaurant_name=b.restaurant_name
+AND a.settlement_year=b.settlement_year
+AND a.settlement_month=b.settlement_month
+AND a.orders_count=b.orders_count
+AND a.orders_total_sum=b.orders_total_sum
+AND a.orders_bonus_payment_sum=b.orders_bonus_payment_sum
+AND a.orders_bonus_granted_sum=b.orders_bonus_granted_sum
+AND a.order_processing_fee=b.order_processing_fee
+AND a.restaurant_reward_sum=b.restaurant_reward_sum
+WHERE a.id IS NULL OR b.id IS null
